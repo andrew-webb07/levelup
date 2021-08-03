d27f639dc9507bdb3672dbebeb8b54af3258c4cf
Andrew Webb <webba@ihopu.org>
2021-08-03 14:27:43 -0500

    first commit

---
+++ levelupapi/models/game.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+    game_type = models.ForeignKey("GameType", on_delete=models.CASCADE)
+    description = models.CharField(max_length=150)
+    number_of_players = models.IntegerField()
+    gamer = models.ForeignKey("Gamer", on_delete=CASCADE)
+    maker = models. CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
