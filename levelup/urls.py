c9e6793d3e54b32247780e4e6da0534b2ce77d59
Andrew Webb <webba@ihopu.org>
2021-08-04 13:35:22 -0500

    can view events

--- levelup/urls.py
+++ levelup/urls.py
@@ -17,11 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
-from levelupapi.views import GameTypeView, GameView, register_user, login_user
+from levelupapi.views import GameTypeView, GameView, register_user, login_user, EventView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypeView, 'gametype')
 router.register(r'games', GameView, 'game')
+router.register(r'events', EventView, 'event')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
