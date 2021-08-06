3f074218d2d2c940cdace7267c06f00b5c90ab49
Andrew Webb <webba@ihopu.org>
2021-08-06 11:46:33 -0500

    added profile view to display current user info and the events they are attending

--- levelup/urls.py
+++ levelup/urls.py
@@ -17,13 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
-from levelupapi.views import GameTypeView, GameView, register_user, login_user, EventView, GamerView
-
+from levelupapi.views import GameTypeView, GameView, register_user, login_user, EventView, GamerView, Profile
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypeView, 'gametype')
 router.register(r'games', GameView, 'game')
 router.register(r'events', EventView, 'event')
 router.register(r'gamers', GamerView, 'gamer')
+router.register(r'profile', Profile, 'profile')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
