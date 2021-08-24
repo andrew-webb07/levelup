d27727dccd481d279df378f89c7678dce32a44e4
Andrew Webb <webba@ihopu.org>
2021-08-24 15:19:00 -0500

    added game-type tests

--- levelupapi/views/game_type.py
+++ levelupapi/views/game_type.py
@@ -1,13 +1,33 @@
 """View module for handling requests about game types"""
+from levelupapi.models.game import Game
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import serializers
-from levelupapi.models import GameType
+from levelupapi.models import GameType, game_type
+from django.core.exceptions import ValidationError
 
 
 class GameTypeView(ViewSet):
     """Level up game types"""
+
+    def create(self, request):
+        """Handle POST operations
+
+        Returns:
+            Response -- JSON serialized game instance
+        """
+        game_type = GameType()
+        game_type.label = request.data["label"]
+
+        try:
+            game_type.save()
+            serializer = GameTypeSerializer(game_type, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
 
     def retrieve(self, request, pk=None):
         """Handle GET requests for single game type
@@ -19,6 +39,9 @@ class GameTypeView(ViewSet):
             game_type = GameType.objects.get(pk=pk)
             serializer = GameTypeSerializer(game_type, context={'request': request})
             return Response(serializer.data)
+
+        except GameType.DoesNotExist as ex:
+            return Response({'message':ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
         except Exception as ex:
             return HttpResponseServerError(ex)
 
@@ -36,6 +59,35 @@ class GameTypeView(ViewSet):
         serializer = GameTypeSerializer(
             game_types, many=True, context={'request': request})
         return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        """Handle PUT requests for an event
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        game_type = GameType.objects.get(pk=pk)
+        game_type.label = request.data["label"]
+        game_type.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single game
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            game_type = GameType.objects.get(pk=pk)
+            game_type.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except GameType.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class GameTypeSerializer(serializers.ModelSerializer):
     """JSON serializer for game types
